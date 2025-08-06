3157ac1e0a678fa06d996b75d1e2eee9350de7df
Thibault Jamet <tjamet@users.noreply.github.com>
2025-08-06 17:35:15 +0200

    Add verbosity to EnsureFolder
    Currently, we face a problem where folders may be re-created
    
    Adding the verbosity helps understand in which conditions they get re-created
    
    Change-Id: Ie1f8074c10e536739e355889a59940a85dfb4cfb

--- client/dashboard.go
+++ client/dashboard.go
@@ -159,6 +159,7 @@ func (sc *StackClient) GetFolder(rootFolder *Folder, folderName string) (*Folder
 	for _, f := range foldersRes.Payload {
 		log.DefaultLogger.WithField("folder", f.Title).WithField("searched", folderName).Tracef("matching folder")
 		if f.Title == folderName {
+			log.DefaultLogger.WithField("folder", f.Title).WithField("searched", folderName).Tracef("found folder")
 			return &Folder{
 				UID:   f.UID,
 				Title: f.Title,
@@ -179,7 +180,7 @@ func (sc *StackClient) EnsureFolder(rootFolder *Folder, folderName string) (*Fol
 		return nil, fmt.Errorf("failed to get folders for %s: %w", folderName, err)
 	}
 
-	log.DefaultLogger.WithField("folder", folder). WithField("searched", folderName). Tracef("found folder")
+	log.DefaultLogger.WithField("folder", folder).WithField("searched", folderName).Tracef("found folder")
 
 	if folder != nil {
 		return folder, nil
